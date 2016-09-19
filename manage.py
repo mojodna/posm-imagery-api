0c3d726396dc4a09fa24c2e6afec8c578030e143
Seth Fitzsimmons <seth@mojodna.net>
2016-09-19 12:37:52 -0700

    Handle int coercions by display raw rule

--- manage.py
+++ manage.py
@@ -14,14 +14,8 @@ def list_routes():
     import urllib
     output = []
     for rule in app.url_map.iter_rules():
-
-        options = {}
-        for arg in rule.arguments:
-            options[arg] = "[{0}]".format(arg)
-
-        methods = ','.join(rule.methods)
-        url = url_for(rule.endpoint, **options)
-        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        line = urllib.unquote("{:20s}\t{}\t{:50s}".format(
+            ','.join(rule.methods), rule.rule, rule.endpoint))
         output.append(line)
 
     for line in sorted(output):
